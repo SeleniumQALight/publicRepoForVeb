392f4e9394f8a16d496e03d2b998f01ba48d4bd5
Muks <muks.bodya@gmail.com>
2023-04-07 16:39:40 +0300

    Added getter and setter

--- Matiashchuk.java
+++ Matiashchuk.java
@@ -7,4 +7,19 @@ public class Matiashchuk {
 		this.age = age;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
