e06fcb6d11084ad2b54e871ab373cfdb0158204c
Taras Radulenko <Taras_Radulenko@epam.com>
2023-04-12 17:21:10 +0300

    negative test

--- radulenkoProj/src/test/java/apiTests/ApiTests.java
+++ radulenkoProj/src/test/java/apiTests/ApiTests.java
@@ -60,4 +60,32 @@ public class ApiTests {
 
     }
 
+    @Test
+    public void negativeGetAllPostsByUserTest(){
+        String actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POST_BY_USER, "NotValidUser")
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response().getBody().asString();
+
+        Assert.assertEquals("Message in response "
+                , "\"Sorry, invalid user requested.undefined\""
+                , actualResponse);
+        Assert.assertEquals("Message in response "
+                , "Sorry, invalid user requested.undefined"
+                , actualResponse.replace("\"",""));
+    }
+
+
+
+
+
+
+
+
 }
