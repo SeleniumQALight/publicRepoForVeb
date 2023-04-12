9de29017e7c75ee09fd501800cb3abb9d46272e6
galmiz.maksym <galmiz.m@gmail.com>
2023-04-12 17:36:54 +0300

    galmiz jPath

--- galmizProj/src/test/java/apiTest/ApiTest.java
+++ galmizProj/src/test/java/apiTest/ApiTest.java
@@ -4,10 +4,14 @@ import api.dto.AuthorDto;
 import api.dto.PostDto;
 import api.endPoints.EndPoints;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.apache.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -73,6 +77,35 @@ public class ApiTest {
         Assert.assertEquals("Message in response "
                 , "\"Sorry, invalid user requested.undefined\""
                 , actualResponse );
+    }
+
+    @Test
+    public void getAllPostsByUserPathTest(){
+        Response actualResponse =
+                given()
+                        .contentType(ContentType.JSON)
+                        .log().all()
+                        .when()
+                        .get(EndPoints.POST_BY_USER, USER_NAME)
+                        .then()
+                        .statusCode(200)
+                        .log().all()
+                        .extract().response();
+
+        List<String> actualTitleList = actualResponse.jsonPath().getList("title", String.class);
+        SoftAssertions softAssertions = new SoftAssertions();
+        for (int i = 0; i < actualTitleList.size(); i++) {
+            softAssertions.assertThat(actualTitleList.get(i)).as("Item number " + i)
+                    .contains("test");
+        }
+
+        List<Map> actualAuthorList = actualResponse.jsonPath().getList("author", Map.class);
+        for (int i = 0; i < actualAuthorList.size(); i++) {
+    softAssertions.assertThat(actualAuthorList.get(i).get("username"))
+            .as("Item number " + i).isEqualTo(USER_NAME);
+        }
+
+        softAssertions.assertAll();
     }
 
 }
