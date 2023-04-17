fd812ca531f4ae3f50209a39c860f33be6d8734c
Anna <anuli4ka80@gmail.com>
2023-04-17 12:25:48 +0300

    update homeTask - add softAssertions+test

--- holubenko_HomeTask_Privat24/src/main/java/api/dto/ExchangeRateDto.java
+++ holubenko_HomeTask_Privat24/src/main/java/api/dto/ExchangeRateDto.java
@@ -14,4 +14,6 @@ import lombok.NoArgsConstructor;
 public class ExchangeRateDto {
     String baseCurrency;
     String currency;
+    Double saleRateNB;
+    Double purchaseRateNB;
 }
--- holubenko_HomeTask_Privat24/src/test/java/apiTests/ApiPrivatTests.java
+++ holubenko_HomeTask_Privat24/src/test/java/apiTests/ApiPrivatTests.java
@@ -5,6 +5,7 @@ import api.dto.PrivatDto;
 import api.endPoints.EndPoints;
 import io.restassured.http.ContentType;
 import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -36,5 +37,68 @@ public class ApiPrivatTests {
         logger.info("BaseCurrencyLit = " + privatDtoResponse.getBaseCurrencyLit());
         logger.info("ExchangeRate = " + privatDtoResponse.getExchangeRate().get(2));
         logger.info("ExchangeRate = " + privatDtoResponse.getExchangeRate().get(10));
+
+        List<ExchangeRateDto> expectedExchangeRateList = List.of(
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("AUD").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("AZN").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("BYN").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("CAD").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("CHF").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("CNY").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("CZK").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("DKK").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("EUR").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("GBP").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("GEL").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("HUF").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("ILS").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("JPY").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("KZT").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("MDL").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("NOK").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("PLN").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("SEK").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("SGD").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("TMT").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("TRY").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("UAH").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("USD").build(),
+                ExchangeRateDto.builder().baseCurrency("UAH").currency("UZS").build()
+        );
+
+        PrivatDto expectedResult =
+                PrivatDto.builder().date("22.03.2022").bank("PB").baseCurrency(980).baseCurrencyLit("UAH")
+                        .exchangeRate(expectedExchangeRateList).build();
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions.assertThat(privatDtoResponse)
+                .usingRecursiveComparison()
+                .ignoringFields("exchangeRate.saleRateNB", "exchangeRate.purchaseRateNB"
+                        , "exchangeRate.purchaseRate", "exchangeRate.saleRate")
+                .isEqualTo(expectedResult);
+
+        softAssertions.assertAll();
+    }
+
+    @Test
+    public void testAllExchangeRateFieldsArePositive() {
+        List<ExchangeRateDto> getExchangeRate = given()
+                .contentType(ContentType.JSON)
+                .queryParam("date", "22.03.2022")
+                .log().all()
+                .when()
+                .get(EndPoints.POST_BY_BANK)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract().jsonPath().getList("exchangeRate", ExchangeRateDto.class);
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (ExchangeRateDto exchangeRate : getExchangeRate) {
+            softAssertions.assertThat(exchangeRate.getSaleRateNB()).isGreaterThan(0);
+            softAssertions.assertThat(exchangeRate.getPurchaseRateNB()).isGreaterThan(0);
+        }
     }
 }
