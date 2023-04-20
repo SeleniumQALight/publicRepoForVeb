428aa7021303cf6e6bfc26ba3c1f1c291723cce2
Anna <anuli4ka80@gmail.com>
2023-04-20 16:04:50 +0300

    added changes to test1

--- holubenko_HomeTask_Privat24/src/test/java/apiTests/ApiPrivatTests.java
+++ holubenko_HomeTask_Privat24/src/test/java/apiTests/ApiPrivatTests.java
@@ -9,6 +9,7 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -38,33 +39,13 @@ public class ApiPrivatTests {
         logger.info("ExchangeRate = " + privatDtoResponse.getExchangeRate().get(2));
         logger.info("ExchangeRate = " + privatDtoResponse.getExchangeRate().get(10));
 
-        List<ExchangeRateDto> expectedExchangeRateList = List.of(
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("AUD").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("AZN").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("BYN").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("CAD").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("CHF").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("CNY").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("CZK").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("DKK").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("EUR").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("GBP").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("GEL").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("HUF").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("ILS").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("JPY").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("KZT").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("MDL").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("NOK").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("PLN").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("SEK").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("SGD").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("TMT").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("TRY").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("UAH").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("USD").build(),
-                ExchangeRateDto.builder().baseCurrency("UAH").currency("UZS").build()
-        );
+        List<ExchangeRateDto> expectedExchangeRateList = new ArrayList<>();
+        String[] currencies = {"AUD", "AZN", "BYN", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "GEL", "HUF", "ILS", "JPY", "KZT", "MDL", "NOK", "PLN", "SEK", "SGD", "TMT", "TRY", "UAH", "USD", "UZS"};
+
+        for (String currency : currencies) {
+            ExchangeRateDto exchangeRateDto = ExchangeRateDto.builder().baseCurrency("UAH").currency(currency).build();
+            expectedExchangeRateList.add(exchangeRateDto);
+        }
 
         PrivatDto expectedResult =
                 PrivatDto.builder().date("22.03.2022").bank("PB").baseCurrency(980).baseCurrencyLit("UAH")
